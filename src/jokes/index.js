3afb377df7dc4dddacd781c36ee394f64f2b6b9e
Rodrigo Sakae <devsakae@gmail.com>
2023-11-27 22:00:04 -0300

    chore: novo local para os audios e envio para todos os grupos

--- src/jokes/index.js
+++ src/jokes/index.js
@@ -88,9 +88,9 @@ const falaPraEle = async (m) => {
   }
 }
 
-const falaAlgumaCoisa = async (m) => {
-  const pack = ['mastella.mp3', 'zecalo.mp3', 'aa1.mp3', 'aa2.mp3', 'aa3.mp3', 'argel.mp3'];
-  const audio = await MessageMedia.fromFilePath('./src/jokes/audios/' + pack[Math.floor(Math.random() * pack.length)])
+const falaAlgumaCoisa = async () => {
+  const pack = ['mastella.mp3', 'zecalo.mp3', 'aa1.mp3', 'aa2.mp3', 'aa3.mp3', 'argel.mp3', 'tencatti.mp3'];
+  const audio = await MessageMedia.fromFilePath('./data/audios/' + pack[Math.floor(Math.random() * pack.length)])
   return Promise.all(Object.keys(config.grupos).map(async grupo => {
     const chat = await client.getChatById(grupo);
     await chat.sendMessage(audio, { sendAudioAsVoice: true });
