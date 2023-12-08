710e437189008d3a71f47ae2ab7e22c34bb9cca4
Rodrigo Sakae <devsakae@gmail.com>
2023-12-08 07:24:13 -0300

    chore: ajustes de português

--- src/futebol/utils/functions.js
+++ src/futebol/utils/functions.js
@@ -29,7 +29,7 @@ const umAtleta = (array) => {
   }, { jogos: 0, gols: 0, v: 0, e: 0, d: 0 });
   const aproveitamento = (((total.v * 3) + (total.e)) / (total.jogos * 3)) * 100
   let response = `O CRAQUE, GÊNIO, LENDÁRIO *${array[0].nickname.toUpperCase()}* jogou por aqui! 🐯\n\n${array[0].name} (${array[0].position}), nascido em ${array[0].birthday}, disputou ${total.jogos} partidas pelo Criciúma Esporte Clube, com aproveitamento de ${aproveitamento.toFixed(1)}%.`;
-  response += `\n\nSua última partida pelo tricolor foi por ${jogos[0].torneio} de ${jogos[0].ano}, tendo ${array[0].nickname} disputado ${jogos[0].jogos} jogos e conquistado ${jogos[0].v} vitórias, ${jogos[0].e} empates e ${jogos[0].d} derrotas (aproveitamento de ${(((Number(jogos[0].v) * 3) + Number(jogos[0].e)) / (Number(jogos[0].jogos) * 3) * 100).toFixed(1)}%.)`
+  response += `\n\nSua última partida pelo tricolor foi por ${jogos[0].torneio} de ${jogos[0].ano}, tendo ${array[0].nickname} disputado ${jogos[0].jogos} jogos e conquistado ${jogos[0].v} vitórias, ${jogos[0].e} empates e ${jogos[0].d} derrotas (aproveitamento de ${(((Number(jogos[0].v) * 3) + Number(jogos[0].e)) / (Number(jogos[0].jogos) * 3) * 100).toFixed(1)}%).`
   if (clubes.length > 0) {
     response += `\n\nAlém do nosso glorioso tricolor, ${array[0].nickname} também jogou contra a gente 😡 vestindo a(s) camisa(s) de `
     clubes.forEach((c, i) => response += `${i === 0 ? '' : i === (clubes.length - 1) ? ' e ' : ', '}${c}${i === (clubes.length - 1) ? '.' : ''}`)
@@ -37,14 +37,9 @@ const umAtleta = (array) => {
   response += `\n\nHistórico completo:`
   array[0].jogos.forEach((jogo) => {
     response += `\n\n➤ *${jogo.torneio}* (${jogo.ano})`;
-    response += `\n🏟 ${jogo.jogos} ${jogo.jogos > 1 ? 'jogos' : 'jogo'} (${jogo.v}V/${jogo.e}E/${jogo.d}D) ⚽️ ${jogo.gols > 0 ? jogo.gols : 'Nenhum'} ${jogo.gols > 1 ? 'gols' : 'gol'}`
+    response += `\n🏟 ${jogo.jogos} ${jogo.jogos > 1 ? 'jogos' : 'jogo'} (${jogo.v}V/${jogo.e}E/${jogo.d}D) ${jogo.gols > 1 ? `⚽️ ${jogo.gols} gols` : jogo.gols === 0 ? '' : '⚽️ 1 gol'}`
     if (!jogo.jogounotigre && jogo.clube) response += ` 👉 ${jogo.clube}`
   });
-  // jogos.forEach((jogo) => {
-  //   response += `\n\n➤ *${jogo.torneio}* (${jogo.ano})`;
-  //   if (!jogo.jogounotigre && jogo.clube) response += `\nAtuando pelo ${jogo.clube}`
-  //   response += `\n🏟 ${jogo.jogos} ${jogo.jogos > 1 ? 'jogos' : 'jogo'} (${jogo.v}V/${jogo.e}E/${jogo.d}D) ⚽️ ${jogo.gols} ${jogo.gols > 1 ? 'gols' : 'gol'}`
-  // });
   response += '\n\nDados: meutimenarede.com.br\nScraped by @devsakae - devsakae.tech/tigrebot'
   return response;
 }
