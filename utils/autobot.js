1e10b3a52da0940ddb8d00dbbe91b59378bfd7bc
Rodrigo Sakae <devsakae@gmail.com>
2023-12-24 10:21:49 -0300

    chore: limpeza de arquivos

--- utils/autobot.js
+++ utils/autobot.js
@@ -4,16 +4,7 @@ const { falaAlgumaCoisa } = require('../src/jokes');
 const { jogadorDoTigreAleatorio, publicaJogoAleatorio } = require('../src/futebol');
 const { atualizaSobreCriciuma } = require('../src/news');
 
-const sortingNumbers = (start, finish) => (Math.floor(Math.random() * (finish - start + 1) + start)).toString();
-const cedo = () => sortingNumbers(2, 42).concat(" ").concat(sortingNumbers(6, 7)).concat(" ");
-const manha = () => sortingNumbers(12, 52).concat(" ").concat(sortingNumbers(8, 10)).concat(" ");
-const tarde = () => sortingNumbers(15, 59).concat(" ").concat(sortingNumbers(14, 16)).concat(" ");
-const noite  = () => sortingNumbers(4, 46).concat(" ").concat(sortingNumbers(18, 20)).concat(" ");
-
-const bomDia = () => {
-  const time = cedo() + "* * *" // Todos os dias, entre 5:02 e 7:32
-  console.log('Cron Schedule BOM DIA:', time)
-  console.log('Validate:', cron.validate(time))
+const bomDia = time => {
   cron.schedule(time, async () => {
     const today = new Date();
     console.info('Rodando BOM DIA - ', today.toLocaleString('pt-br'));
@@ -74,8 +65,4 @@ module.exports = {
   atletaDestaque,
   googleNewsCriciuma,
   jogosHistoricos,
-  cedo,
-  manha,
-  tarde,
-  noite,
 }
