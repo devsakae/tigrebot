7538f3a70c3f5aea9cab004038546db55810e2be
Rodrigo Sakae <devsakae@gmail.com>
2024-03-09 17:43:33 -0300

    temp fix: clear messages de chats

--- src/connections.js
+++ src/connections.js
@@ -67,9 +67,11 @@ client.on('ready', async () => {
     .map(async (group) => {
       if (Object.hasOwn(config.grupos, group.id_serialized) && config.groups[group.id_serialized]?.palpiteiros.length > 0) return '';
       if (group.id._serialized.endsWith('-1401890927@g.us')) return '';
+      if (group.id._serialized.includes('newsletter')) return '';
+      await group.clearMessages();
       config.grupos[group.id._serialized] = { palpiteiros: [] };
-      const totalMessages = await group.fetchMessages({ limit: 10 });
-      await Promise.all(totalMessages.filter(m => m.ack === 1).map(async m => await group.sendSeen(m.id._serialized)))
+      // const totalMessages = await group.fetchMessages({ limit: 10 });
+      // await Promise.all(totalMessages.filter(m => m.ack === 1).map(async m => await group.sendSeen(m.id._serialized)))
       console.log('✔️ ', group.name, '[grupo]');
     }));
   fs.writeFileSync(
