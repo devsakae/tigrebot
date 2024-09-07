d06ea30b61822e61971e5fe05b621a80568e802e
Rodrigo Sakae <devsakae@gmail.com>
2024-09-07 11:10:09 -0300

    fix: instagram api funcional

--- src/instagram/index.js
+++ src/instagram/index.js
@@ -11,24 +11,26 @@ const options = {
 };
 
 const instagram = async (m) => {
-  log_info("Fetching instagram! Aguarde...");
   if (m.from === process.env.BOT_OWNER && m.body.includes("instagram.com")) {
+    log_info("Fetching instagram! Aguarde...");
     const splitado = m.body.split('/');
     instaurl = url + (splitado[3] === "p" ? splitado[4] : m.body) + '&include_insights=true';
     try {
       const response = await fetch(instaurl, options);
-      const { data } = await response.text();
-      let caption = data.caption.text;
-      caption += `\n📸 ${data.user.username} (${data.user.full_name})`;
-      caption += `\n💛 ${data.metrics.like_count} curtidas 👁‍🗨 ${data.metrics.comment_count} comentários`;
-      caption += `\nCapturado e enviado até você por TigreBot - ${config.mysite}`;
-      sendMediaUrlToGroups({ url: data.image_versions.items[-1].url, caption: caption })
+      const res = await response.text();
+      let raw_caption = res.data.caption.text;
+      raw_caption += `\n📸 ${res.data.user.username} (${res.data.user.full_name})`;
+      raw_caption += `\n💛 ${res.data.metrics.like_count} curtidas 👁‍🗨 ${res.data.metrics.comment_count} comentários`;
+      raw_caption += `\nCapturado e enviado até você por TigreBot - ${config.mysite}`;
+      const image_versions = resposta.data.image_versions.items
+      const imagem = image_versions[image_versions.length - 1].url;
+      sendMediaUrlToGroups({ url: imagem, caption: raw_caption })
     } catch (error) {
       console.error(error);
-    }  
+    }
   }
 }
 
 module.exports = {
-  instagram
+  instagram,
 }
