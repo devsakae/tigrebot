ed47dea75ed0087959da5c14797cef7e91eb61b0
Rodrigo Sakae <devsakae@gmail.com>
2024-11-03 08:35:54 -0300

    preparativos para bot em outra versão

--- utils/sender.js
+++ utils/sender.js
@@ -3,32 +3,34 @@ const config = require('../data/tigrebot.json');
 const { MessageMedia } = require('whatsapp-web.js');
 const { site_publish } = require('./mongo');
 
+const notDefendeAi = (dest) => (dest !== process.env.DEFENDE_AI);
+
 const sendTextToGroups = async text => {
   for (dest of Object.keys(config.grupos)) {
-    await client.sendMessage(dest, text)
+    if (notDefendeAi(dest)) await client.sendMessage(dest, text)
   }
   await site_publish(text);
 }
 
 const sendTextToChannels = async text => {
   for (dest of Object.keys(config.canais)) {
-    await client.sendMessage(dest, text)
+    if (notDefendeAi(dest)) await client.sendMessage(dest, text)
   }
   await site_publish(text);
 }
 
 const echoToGroups = async text => {
-  await Promise.all(Object.keys(config.grupos).map(async g => await client.sendMessage(g, text)));
+  await Promise.all(Object.keys(config.grupos).map(async g => {if (notDefendeAi(g)) await client.sendMessage(g, text)}));
   await site_publish(text);
 }
 const echoToChannel = async text => {
-  await Promise.all(Object.keys(config.canais).map(async c => await client.sendMessage(c, text)));
+  await Promise.all(Object.keys(config.canais).map(async c => { if (notDefendeAi(c)) await client.sendMessage(c, text)}));
   await site_publish(text);
 }
 
 const sendBolaoGroups = async text => {
   for (grupo of Object.keys(config.bolao.grupos)) {
-    await client.sendMessage(grupo, text);
+    if (notDefendeAi(grupo)) await client.sendMessage(grupo, text);
   }
   await site_publish(text);
 };
@@ -36,7 +38,7 @@ const sendBolaoGroups = async text => {
 const sendMediaUrlToGroups = async media => {
   const mediaFile = await MessageMedia.fromUrl(media.url);
   for (grupo of Object.keys(config.grupos)) {
-    await client.sendMessage(grupo, mediaFile, { caption: media.caption })
+    if (notDefendeAi(grupo)) await client.sendMessage(grupo, mediaFile, { caption: media.caption })
   }
   await site_publish(media.caption);
 }
@@ -53,7 +55,7 @@ const sendInstagramToGroups = async media => {
   const mediaFile = await MessageMedia.fromUrl(media.url);
   const newCaption = media.caption + '\n\n📷 @' + media.owner + '\n🔗 ' + media.link + '\nCapturado e enviado até você por TigreBot ' + config.mysite;
   for (grupo of Object.keys(config.grupos)) {
-    await client.sendMessage(grupo, mediaFile, { caption: newCaption });
+    if (notDefendeAi(grupo)) await client.sendMessage(grupo, mediaFile, { caption: newCaption });
   }
   await site_publish(newCaption);
 }
