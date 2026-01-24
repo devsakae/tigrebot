f293604d44fd0f34ad89441d2980745191a13992
Rodrigo Sakae <devsakae@gmail.com>
2026-01-24 15:41:34 -0300

    rebase: version and minor adjustments

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\n### Bem vindo ao TigreBot v32026 ###"
+printf "\n### Bem vindo ao TigreBot v2026 ###"
 printf "\nAcesse http://www.devsakae.com.br/tigrebot para mais informações"
 printf "\n\nIniciando sistema..."
 chmod 664 ./data/tigrebot.json
